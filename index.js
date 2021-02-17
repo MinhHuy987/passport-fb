eff25948a087e8de3a32edd087f69c49eafc1069
Huy <beatys987@gmail.com>
2021-02-17 16:52:27 +0700

    initial commit

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.get('/auth/facebook',passport.authenticate('facebook'))
 app.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/login' }),
 function(req, res) {
   // Successful authentication, redirect home.
-  res.redirect('/');
+  res.redirect('/home');
 });
 app.get('/home',(req,res,next)=>{
     res.json(req.user);
